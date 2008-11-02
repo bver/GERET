d436f2a0174d30c54c6ccc3c4f056536cabd5ba9
bver <pavel.suchmann@gmail.com>
2008-11-02 09:46:50 +0100

    more tests

--- rgel/lib/random.rb
+++ rgel/lib/random.rb
@@ -1,4 +1,43 @@
 
 class Random
+  def initialize mode
+
+    @core = proc {|max| Kernel::rand max }
+
+    case mode
+
+    when :stochastic
+      srand
+
+    when :repeatable
+      srand 42
+
+    when :deterministic
+      @predef = nil
+      @core = proc do |max|
+        raise 'Random: set() in :deterministic mode not called' if @predef.nil?
+        raise 'Random: shortage of :deterministic values' if @predef.empty?
+
+        max = 1.0 if max == 0
+        raise 'Random: :deterministic value exceeded' if @predef.first > max 
+
+        return @predef.shift
+      end
+
+    else
+      raise 'Random: mode not supported'
+
+    end
+  end
+
+  attr_reader :predef 
+
+  def set arg
+    @predef = Array.new arg
+  end
+
+  def rand max=0
+    @core.call(max)
+  end
 end
 
