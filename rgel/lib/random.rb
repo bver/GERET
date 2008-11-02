9987b8483133a98e2f4ffc07c326a82e983e38d9
bver <pavel.suchmann@gmail.com>
2008-11-02 12:32:58 +0100

    rdoc for Random

--- rgel/lib/random.rb
+++ rgel/lib/random.rb
@@ -1,5 +1,18 @@
 
+# Random class is basically a wrapper over Kernel::rand() function, supporting
+# also a special :deterministic mode, which is helpful for unit testing.
+#
 class Random
+
+  # creates a new random generator
+  # - mode can be :stochastic, :repeatable or :deterministic
+  #
+  # :stochastic - classic production mode, generating random numbers as Kernel::rand does
+  #
+  # :repeatable - same as :stochastic but producing same sequences, initialized by Kernel::srand(42)
+  #
+  # :deterministic - producing predefined sequences, user needs to call set_predef() first
+  #
   def initialize mode
 
     @core = proc {|max| Kernel::rand max }
@@ -30,12 +43,24 @@ class Random
     end
   end
 
+  # gets the remaining sequence for rand() calls (useful only in :deterministic mode)
   attr_reader :predef 
 
+  # when in :deterministic mode, this method assigns the predefined sequence to the generator.
+  #
+  # - arg is the sequence (array) of numbers for rand() calls
+  #
+  # arg.first will be the result subsequent rand() call, arg[1] will be the second one, etc.
+  #
   def set_predef arg
     @predef = Array.new arg
   end
 
+  # in :stochastic or :repeatable mode, it is same as Kernel::rand(); 
+  # see http://www.rubycentral.com/book/ref_m_kernel.html#Kernel.rand 
+  #
+  # in :deterministic mode, it follows the predefined sequence.
+  #
   def rand max=0
     @core.call(max)
   end
