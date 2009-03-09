f5547fb3242ba5d0e4b112cd13dd8cdab72612df
bver <pavel.suchmann@gmail.com>
2009-03-09 23:15:23 +0100

    margin implemented

--- geret/lib/crossover.rb
+++ geret/lib/crossover.rb
@@ -1,19 +1,22 @@
 
-class GenOps
+class Crossover
   def initialize
     @random = Kernel
+    @margin = 0
   end
 
-  attr_accessor :random
+  attr_accessor :random, :margin
 
   def crossover( parent1, parent2 )
-    pt1 = @random.rand(parent1.size+1)
-    pt2 = @random.rand(parent2.size+1)
+    return parent1.clone, parent2.clone if parent1.size < 2*@margin or parent2.size < 2*@margin
+
+    pt1 = @random.rand(parent1.size+1 - 2*@margin) + @margin 
+    pt2 = @random.rand(parent2.size+1 - 2*@margin) + @margin
 
     offs1 = parent1[0...pt1].clone
     offs1 = offs1.concat parent2[pt2...parent2.size]
     offs2 = parent2[0...pt2].clone   
-    offs2 = offs2.concat parent1[pt1...parent2.size]
+    offs2 = offs2.concat parent1[pt1...parent1.size]
     return offs1, offs2
   end
 end
--- geret/test/tc_crossover.rb
+++ geret/test/tc_crossover.rb
@@ -10,10 +10,10 @@ class TC_Crossover < Test::Unit::TestCase
     parent1 = [1, 2,    3, 4, 5]
     parent2 = [6, 7, 8, 9, 10,   11, 12]
 
-    ops = GenOps.new
+    c = Crossover.new
     r = MockRand.new [{6,2}, {8,5}]      
-    ops.random = r
-    offspring1, offspring2 = ops.crossover( parent1, parent2 ) 
+    c.random = r
+    offspring1, offspring2 = c.crossover( parent1, parent2 ) 
 
     assert_equal( [1, 2, 11, 12], offspring1 )
     assert_equal( [6, 7, 8, 9, 10, 3, 4, 5], offspring2 )   
@@ -26,22 +26,59 @@ class TC_Crossover < Test::Unit::TestCase
     parent1 = [1  ]
     parent2 = [2, 3, 4, 5,    6, 7, 8, 9]
 
-    ops = GenOps.new
+    c = Crossover.new
     r = MockRand.new [{2,1}, {9,4}]      
-    ops.random = r
-    offspring1, offspring2 = ops.crossover( parent1, parent2 ) 
+    c.random = r
+    offspring1, offspring2 = c.crossover( parent1, parent2 ) 
 
     assert_equal( [1, 6, 7, 8, 9], offspring1 )
     assert_equal( [2, 3, 4, 5], offspring2 )   
 
     r = MockRand.new [{2,0}, {9,8}]      
-    ops.random = r
-    offspring1, offspring2 = ops.crossover( parent1, parent2 ) 
+    c.random = r
+    offspring1, offspring2 = c.crossover( parent1, parent2 ) 
 
     assert_equal( [], offspring1 )
     assert_equal( [2, 3, 4, 5,    6, 7, 8, 9, 1], offspring2 )   
   end
 
+  def test_margin
+    parent1 = [1, 2, 3,   4, 5]
+    parent2 = [6, 7, 8, 9,    10, 11, 12]
+
+    c = Crossover.new
+    r = MockRand.new [{2,1}, {4,2}]      
+    c.random = r
+
+    assert_equal( 0, c.margin )
+    c.margin = 2
+    assert_equal( 2, c.margin )
+
+    offspring1, offspring2 = c.crossover( parent1, parent2 ) 
+
+    assert_equal( [1, 2, 3, 10, 11, 12], offspring1 )
+    assert_equal( [6, 7, 8, 9, 4, 5], offspring2 )   
+
+    r = MockRand.new []      
+    c.random = r
+
+    c.margin = 3
+    assert_equal( 3, c.margin )
+
+    offspring1, offspring2 = c.crossover( parent1, parent2 ) 
+    assert_equal( parent1, offspring1 )
+    assert_equal( parent2, offspring2 )
+
+    parent1 = [1, 2, 3,   4, 5, 6]
+    r = MockRand.new [{1,0}, {2,0}]      
+    c.random = r
+   
+    offspring1, offspring2 = c.crossover( parent1, parent2 )
+    assert_equal( [1, 2, 3,  9, 10, 11, 12], offspring1 )
+    assert_equal( [6, 7, 8,  4, 5, 6], offspring2 )   
+
+  end
+ 
  
 end
 
