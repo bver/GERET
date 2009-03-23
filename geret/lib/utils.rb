5e17c14ef5f33ef8d62f870364a526be6d736356
bver <pavel.suchmann@gmail.com>
2009-03-24 00:17:53 +0100

    utils

---
+++ geret/lib/utils.rb
@@ -0,0 +1,41 @@
+
+module Utils
+ 
+  def Utils.statistics arr
+    max = nil
+    min = nil
+    sum = 0.0
+    n = 0
+
+    arr.each do |i|
+      next if i.nil?
+      value = (block_given?) ? (yield i) : (i)
+      max = (max.nil? || value>max) ? value : max
+      min = (min.nil? || value<min) ? value : min
+      sum += value
+      n += 1
+    end
+
+    return min, max, sum/n, n    if n>0  
+    return nil, nil, nil, 0
+  end
+
+  def Utils.diversity arr
+    count = Hash.new 0
+    if block_given?
+      arr.each { |val| count[ yield(val) ] += 1 }
+    else
+      arr.each { |val| count[ val ] += 1 }
+    end
+    count.values.sort {|a,b| b <=> a}
+  end
+
+  def Utils.percent( nominator, denominator )
+    if denominator == 0
+      'N/A%'
+    else
+      "#{(100.0*nominator/denominator).round}%"
+    end
+  end
+ 
+end
---
+++ geret/test/tc_utils.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/ruby
+
+require 'test/unit'
+require 'lib/utils'
+
+class TC_Utils < Test::Unit::TestCase
+
+  def test_stats
+    a = [3, 3, nil, 4, nil, 4, 2]
+    assert_equal( [2, 4, 16.0/5, 5], Utils.statistics(a) )
+
+    n = [nil, nil]
+    assert_equal( [nil, nil, nil, 0], Utils.statistics(n) )
+  end
+  
+  def test_diversity
+    a = [3, 3, nil, 4, 5, 4, 2, 4]
+    assert_equal( [3, 2, 1, 1, 1], Utils.diversity(a) )
+
+    a = [3, -3, 4, 5, -4, 2, -4]
+    assert_equal( [3, 2, 1, 1], Utils.diversity(a) {|a| a.abs} )
+  end
+
+  def test_percent
+    assert_equal( 'N/A%', Utils.percent(1,0) )
+    assert_equal( '10%', Utils.percent(1,10) )
+  end
+end
+
