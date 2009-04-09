4bfe349bc97843bcc21f8a8cc631bc37dbbdf42a
bver <pavel.suchmann@gmail.com>
2009-04-09 07:21:30 +0200

    better tournament args

--- geret/lib/tournament.rb
+++ geret/lib/tournament.rb
@@ -5,21 +5,22 @@ module Selection
 
   class Tournament
 
-    def initialize ranker, pressure_modifier=1.0
+    def initialize ranker, tournament_size, pressure_modifier=1.0
       raise "Tournament: invalid Ranking object" unless ranker.kind_of? Ranking 
       @ranker = ranker
+      @tournament_size = tournament_size 
       @random = Kernel
       @pressure_modifier = pressure_modifier
     end
 
-    attr_accessor :ranker, :random, :pressure_modifier
+    attr_accessor :tournament_size, :ranker, :random, :pressure_modifier
 
-    def select population, tournament_size
+    def select population
       ranked = @ranker.rank population
-      raise "Tournament: tournament_size bigger than population.size" if tournament_size > population.size 
+      raise "Tournament: tournament_size bigger than population.size" if @tournament_size > population.size 
 
-      for rank in ( 0 ... tournament_size )
-        next if @random.rand > @pressure_modifier and rank < tournament_size-1
+      for rank in ( 0 ... @tournament_size )
+        next if @random.rand > @pressure_modifier and rank < @tournament_size-1
         selection = ranked.find_all { |individual| individual.rank == rank }
         return selection[ @random.rand(selection.size) ].original
       end
--- geret/test/tc_tournament.rb
+++ geret/test/tc_tournament.rb
@@ -25,69 +25,81 @@ class TC_Tournament < Test::Unit::TestCase
 
   def test_basic
     r = Ranking.new :fitness 
-    t = Tournament.new r
+    t = Tournament.new( r, 2 )
     t.random = MockRand.new [{0=>0.8}, {1=>0}]
 
     assert_equal( r.object_id, t.ranker.object_id )
 
-    winner = t.select( @population, 2 )
+    winner = t.select @population 
     assert_equal( @population[1].object_id, winner.object_id )
   end
 
   def test_two_candidates
     r = Ranking.new :fitness, :minimize 
-    t = Tournament.new r
+    t = Tournament.new( r, 3 )
 
     t.random = MockRand.new [{0=>0.8}, {2=>1}]
-    winner = t.select( @population, 3 )
+    winner = t.select @population
     assert_equal( @population[3].object_id, winner.object_id )
 
     t.random = MockRand.new [{0=>0.8}, {2=>0}]
-    winner = t.select( @population, 3 )
+    winner = t.select @population
     assert_equal( @population[0].object_id, winner.object_id )
   end
 
   def test_pressure_modifier
     r = Ranking.new :fitness 
-    t = Tournament.new r   
+    t = Tournament.new( r, 3 )   
     assert_equal( 1.0, t.pressure_modifier )
     t.pressure_modifier = 0.5
     assert_equal( 0.5, t.pressure_modifier )
 
-    t = Tournament.new( r, 0.7 )
+    t = Tournament.new( r, 3, 0.7 )
     assert_equal( 0.7, t.pressure_modifier )
     t.random = MockRand.new [{0=>0.8}, {0=>0.5}, {1=>0} ] 
 
-    winner = t.select( @population, 3 )
+    winner = t.select @population
     assert_equal( @population[2].object_id, winner.object_id )
+  end
+
+  def test_tour_size
+    r = Ranking.new :fitness 
+    t = Tournament.new( r, 42 )
+    assert_equal( 42, t.tournament_size )
+    t.tournament_size = 2 
+    assert_equal( 2, t.tournament_size )  
+
+    t.random = MockRand.new [{0=>0.8}, {1=>0}]
+    winner = t.select( @population )
+    assert_equal( @population[1].object_id, winner.object_id )
   end
 
   def test_not_lucky
     r = Ranking.new :fitness 
-    t = Tournament.new( r, 0.7 )
+    t = Tournament.new( r, 2, 0.7 )
     assert_equal( 0.7, t.pressure_modifier )
     t.random = MockRand.new [{0=>0.8}, {0=>0.9}, {1=>0} ] 
 
-    winner = t.select( @population, 2 )
+    winner = t.select @population
     assert_equal( @population[2].object_id, winner.object_id )
   end
 
   def test_too_big_tournament_size
     r = Ranking.new :fitness 
-    t = Tournament.new r
-    exception = assert_raise( RuntimeError ) { t.select( @population, 5 ) }
+    t = Tournament.new( r, 5 )
+    exception = assert_raise( RuntimeError ) { t.select( @population ) }
     assert_equal( "Tournament: tournament_size bigger than population.size", exception.message )
   end
 
   def test_empty_population
     r = Ranking.new :fitness 
-    t = Tournament.new r
-    exception = assert_raise( RuntimeError ) { t.select( [], 5 ) }
+    t = Tournament.new( r, 5 )
+    exception = assert_raise( RuntimeError ) { t.select( [] ) }
     assert_equal( "Ranking: empty population", exception.message )
   end
 
   def test_invalid_ranker
-    exception = assert_raise( RuntimeError ) { Tournament.new 42 }
+    exception = assert_raise( RuntimeError ) { Tournament.new( 42, 2 ) }
     assert_equal( "Tournament: invalid Ranking object", exception.message )
   end
   
