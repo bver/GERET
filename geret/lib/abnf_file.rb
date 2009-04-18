2be66ce1032fa4ea52b48f6d5dcd2b28b0b181d1
pavel <pavel@hodjuk.(none)>
2009-04-18 04:32:49 +0200

    AbnfFile#filename implemented

--- geret/lib/abnf_file.rb
+++ geret/lib/abnf_file.rb
@@ -4,11 +4,29 @@ require 'lib/abnf_tokenizer'
 
 class AbnfFile < Mapper::Grammar
 
-  def initialize filename
-    input = IO.read( filename )
-    stream = Abnf::Tokenizer.new.tokenize( input )
-    grammar = Abnf::Parser.new.parse( stream )
+  def initialize fname=nil
+    grammar = load fname
     super( grammar, grammar.start_symbol )
+  end
+
+  attr_reader :filename
+
+  def filename=( fname )
+    grammar = load fname
+    clear
+    update grammar
+    self.start_symbol = grammar.start_symbol
+  end
+
+  protected
+
+  def load fname
+    @filename = fname
+    return Grammar.new if fname.nil?
+
+    input = IO.read( fname )
+    stream = Abnf::Tokenizer.new.tokenize( input )
+    return Abnf::Parser.new.parse( stream )
   end
 
 end
--- geret/test/tc_abnf_file.rb
+++ geret/test/tc_abnf_file.rb
@@ -5,8 +5,8 @@ require 'lib/abnf_file'
 
 class TC_AbnfFile < Test::Unit::TestCase
 
-  def test_basic
-    grammar_ref = Grammar.new( { 
+  def setup
+    @grammar_ref = Grammar.new( { 
       'expr' => Rule.new( [ 
                   RuleAlt.new( [ Token.new( :literal, 'x' ) ] ),
                   RuleAlt.new( [ Token.new( :literal, 'y' ) ] ),
@@ -24,10 +24,28 @@ class TC_AbnfFile < Test::Unit::TestCase
                   RuleAlt.new( [ Token.new( :literal, '*' ) ] )
                 ] )
     }, 'expr' )
-   
-    grammar_file = AbnfFile.new 'test/data/simple_file.abnf'   
-    
-    assert_equal( grammar_ref, grammar_file )
   end
+
+  def test_basic
+    grammar_file = AbnfFile.new 'test/data/simple_file.abnf'   
+    assert_equal( @grammar_ref, grammar_file )
+  end
+
+  def test_filename_attr
+    grammar_file = AbnfFile.new
+    assert_equal( nil, grammar_file.filename )
+    assert_equal( [], grammar_file.symbols )
+    assert_equal( nil, grammar_file.start_symbol )
+
+    grammar_file.filename = 'test/data/simple_file.abnf'
+    assert_equal( 'test/data/simple_file.abnf', grammar_file.filename )
+    assert_equal( @grammar_ref, grammar_file )  
+
+    grammar_file.filename = nil
+    assert_equal( nil, grammar_file.filename )
+    assert_equal( [], grammar_file.symbols )
+    assert_equal( nil, grammar_file.start_symbol )
+  end
+
 end
 
