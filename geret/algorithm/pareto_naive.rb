d232e9f168b231874ff57fdacfcd843aafbec2b7
Pavel Suchmann <pavel.suchmann@gmail.com>
2009-05-28 07:33:40 +0200

    uniq cropping

--- geret/algorithm/pareto_naive.rb
+++ geret/algorithm/pareto_naive.rb
@@ -1,9 +1,36 @@
 
 require 'algorithm/algorithm_base'
 
+class CroppingIndividual < Struct.new( :orig, :uniq )
+  @@uniq = {}
+  @@uniq.default = 0
+
+  def CroppingIndividual.uniq_clear 
+    @@uniq.clear
+  end
+
+  def initialize( orig ) 
+    super
+    @@uniq[orig.phenotype] += 1
+  end
+
+  def cache_uniq
+    self.uniq = @@uniq[orig.phenotype]
+  end
+ 
+  def <=>(other)
+    if self.uniq < other.uniq
+      return -1
+    else
+      return 1 if other.uniq < self.uniq
+      return 0
+    end
+  end
+end
+
 class ParetoNaive < AlgorithmBase
  
-  attr_accessor :init_size, :mutation_probability
+  attr_accessor :init_size, :mutation_probability, :max_archive_size
 
   def setup config
     super
@@ -12,27 +39,33 @@ class ParetoNaive < AlgorithmBase
 
     @tourney = @cfg.factory( 'tourney' )
 
-    @population, @elite = @store.load
+    @archive, @population = @store.load
+    @archive = [] if @archive.nil?
     @population = [] if @population.nil?
-    @elite = {} if @elite.nil?
 
     @report << "loaded #{@population.size} population individuals"   
     @report << "creating #{@population_size - @population.size} population individuals"
     init_population( @population, @population_size )
-    @report << "loaded #{@elite.size} elite individuals"
+    @report << "loaded #{@archive.size} archive individuals"
 
     @report.next    
     return @report 
   end
 
+  def teardown
+    @report << "--------- finished:"
+    @store.save [@archive, @population]
+    return @report   
+  end
+  
   def step
     @report.next    
     @report << "--------- step #{@steps += 1}" 
    
-    # parents come from elite and previous population:
+    # parents come from the archive and the previous population:
     parents = []
     parents.concat @tourney.select_front( @population ) while parents.size < @population_size
-    parents.concat @elite.values  
+    parents.concat @archive  
     
     # exploration part of the population
     new_population = []
@@ -55,16 +88,15 @@ class ParetoNaive < AlgorithmBase
     # next generation
     @population = new_population
 
-    # update elite
-    new_population.concat @elite.values
-    @elite = {}
-    ParetoTourney.front( new_population ).each do |individual|
-      individual.shorten_chromozome = true
-      @elite[ individual.genotype ] = individual
-    end
+    # update archive
+    new_population.concat @archive
+    CroppingIndividual.uniq_clear 
+    cropping = ParetoTourney.front( new_population ).map { |individual| CroppingIndividual.new( individual ) }
+    cropping.each { |crop| crop.cache_uniq }
+    @archive = cropping.sort[ 0...@max_archive_size ].map { |crop| crop.orig }
 
     # reporting
-    @report.report @elite.values
+    @report.report @archive
  
     return @report 
   end
