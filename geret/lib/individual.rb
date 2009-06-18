09420bcc1b3494c48c281bd471987c50404cf8c4
pavel <pavel@mapfactor.com>
2009-06-18 14:00:39 +0200

    commented

--- geret/lib/individual.rb
+++ geret/lib/individual.rb
@@ -1,10 +1,14 @@
 
 require 'lib/shorten'
 
+# The superclass that wraps genotype and phenotype mapping process into the single instance, typically subclassed by the real-world task individuals.
+# The Individual class provides "duck-type interface" which separates a task from various algorithms.
+#
 class Individual
 
   @@shortener = Shorten.new
 
+  # Create the new phenotype, based on the genotype, using the mapper.
   def initialize( mapper, genotype )
     @genotype = genotype
     @used_length = nil
@@ -15,14 +19,24 @@ class Individual
     @used_length = mapper.used_length 
   end
 
-  attr_reader :genotype, :phenotype, :used_length 
+  # Genotype vector (array of codons) as provided by the constructor.
+  attr_reader :genotype 
 
+  # Phenotype text (string representing typically the source of the program), the result of GP mapping.
+  # Phenotype attribute can be nil (if mapping process fails).
+  attr_reader :phenotype 
+
+  # The number of codons used in the mapping process (see Mapper::Base#used_length).
+  attr_reader :used_length   
+
+  # If set to true, the genome is shortened by the Shorten#shorten with the used_length argument.
   def shorten_chromozome=( shorten )
     return unless shorten
     return if @used_length.nil?
     @genotype = @@shortener.shorten( @genotype, @used_length )
   end
 
+  # Return true if the phenotype is valid (the mapping process succeeded). 
   def valid?
     not self.phenotype.nil?
   end
