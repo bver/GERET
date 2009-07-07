1f07bc3dafab9e7aa2c9575db168c31a09298ca0
pavel <pavel@mapfactor.com>
2009-07-07 11:18:37 +0200

    commenting

--- geret/lib/select_more.rb
+++ geret/lib/select_more.rb
@@ -1,13 +1,21 @@
 
+# Helper module for various Selection methods.
+# It provides unique_winners atribute support and the select method.
 module SelectMore
 
+  # Specify if the selection is without replacement (true means the results of 
+  # the selection results are unique, false means the repetitions may occur).
   attr_writer :unique_winners
 
+  # See unique_winners attribute.
   def unique_winners
     @unique_winners = false unless defined? @unique_winners
     @unique_winners
   end
 
+  # Select more individuals from the population, assuming the user class provides select_one and
+  # select_one_internal methods. 
+  # It can be specified how_much individuals will be selected.
   def select( how_much, population=self.population )
     winners = [ select_one( population ) ]
     ids = [ winners.first.object_id ]
--- geret/lib/shorten.rb
+++ geret/lib/shorten.rb
@@ -1,12 +1,27 @@
 
+# Cut off the unused tail of the chromozome.
+# This operator is typically applied after the GE mapping (see Mapping::Base) when there is 
+# an information about the numbers of codons used for the phenotype creation.
+# There are two modes of the operation: stochastic and deterministic.
+#
 class Shorten
+
+  # Use the default Kernel.rand and deterministic mode.
   def initialize
     @random = Kernel
     @stochastic = false
   end
   
-  attr_accessor :random, :stochastic
+  # the source of randomness, used for calling "random.rand( limit )", defaulting to 'Kernel' class.
+  attr_accessor :random
+ 
+  # The mode of the operation:
+  #    true .. the genotype is shortened at the precisely specified cutting index.
+  #    false .. the genotype is shortened at the random point after the specified index.
+  attr_accessor :stochastic
 
+  # Shorten the genotype gen, specifying the point max.
+  # See stochastic attribute for meaning of the argument max.
   def shorten( gen, max )
     return gen.clone if gen.size <= max
     point = @stochastic ? (@random.rand( gen.size+1-max ) + max) : max
