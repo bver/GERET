793ec0cba3bc089f8a95998c63ba1fee2b191a84
pavel <pavel@mapfactor.com>
2009-07-07 14:59:00 +0200

    Utils -> Util

--- geret/sample/toy_regression/toy_report.rb
+++ geret/sample/toy_regression/toy_report.rb
@@ -2,25 +2,25 @@
 class ToyReport < ReportText 
 
   def report population
-    diversity = Utils.diversity( population ) { |individual| individual.genotype }
+    diversity = Util.diversity( population ) { |individual| individual.genotype }
     self['diversity_genotypic'] << diversity[0...10].inspect   
     
 #    diversity =  Utils.diversity( population ) { |individual| individual.phenotype }
 #    self['diversity_phenotypic'] << diversity[0...10].inspect
 
     errors = population.map { |individual| individual.error }   
-    min, max, avg, n = Utils.statistics( errors.find_all { |e| e.infinite?.nil? } )
+    min, max, avg, n = Util.statistics( errors.find_all { |e| e.infinite?.nil? } )
     self['error_min'] << min
     self['error_max'] << max
     self['error_avg'] << avg
     self['error_finites'] << n 
 
-    min, max, avg, n = Utils.statistics( population.map { |individual| individual.used_length } )
+    min, max, avg, n = Util.statistics( population.map { |individual| individual.used_length } )
     self['usedlen_min'] << min
     self['usedlen_max'] << max
     self['usedlen_avg'] << avg
 
-    min, max, avg, n = Utils.statistics( population.map { |individual| individual.genotype.size } )
+    min, max, avg, n = Util.statistics( population.map { |individual| individual.genotype.size } )
     self['gensize_min'] << min
     self['gensize_max'] << max
     self['gensize_avg'] << avg
--- geret/test/tc_utils.rb
+++ geret/test/tc_utils.rb
@@ -8,31 +8,31 @@ class TC_Utils < Test::Unit::TestCase
 
   def test_stats
     a = [3, 3, nil, 4, nil, 4, 2]
-    assert_equal( [2, 4, 16.0/5, 5], Utils.statistics(a) )
+    assert_equal( [2, 4, 16.0/5, 5], Util.statistics(a) )
 
     n = [nil, nil]
-    assert_equal( [nil, nil, nil, 0], Utils.statistics(n) )
+    assert_equal( [nil, nil, nil, 0], Util.statistics(n) )
 
     a = ['3', '4', '2']
-    assert_equal( [2, 4, 3.0, 3], Utils.statistics(a) { |item| item.to_i } )   
+    assert_equal( [2, 4, 3.0, 3], Util.statistics(a) { |item| item.to_i } )   
   end
   
   def test_diversity
     a = [3, 3, nil, 4, 5, 4, 2, 4]
-    assert_equal( [3, 2, 1, 1, 1], Utils.diversity(a) )
+    assert_equal( [3, 2, 1, 1, 1], Util.diversity(a) )
 
     a = [3, -3, 4, 5, -4, 2, -4]
-    assert_equal( [3, 2, 1, 1], Utils.diversity(a) { |item| item.abs } )
+    assert_equal( [3, 2, 1, 1], Util.diversity(a) { |item| item.abs } )
   end
 
   def test_percent
-    assert_equal( 'N/A%', Utils.percent(1,0) )
-    assert_equal( '10%', Utils.percent(1,10) )
+    assert_equal( 'N/A%', Util.percent(1,0) )
+    assert_equal( '10%', Util.percent(1,10) )
   end
 
   def test_permutation
     r = MockRand.new [ {5=>3}, {4=>1}, {3=>1}, {2=>0}, {1=>0} ]
-    p = Utils.permutate( [1, 2, 3, 4, 5], r )
+    p = Util.permutate( [1, 2, 3, 4, 5], r )
     assert_equal( [4, 2, 3, 1, 5], p )
   end
 
