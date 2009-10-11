a0f05d677991dc585d8a352b32791fbced57b7e4
Pavel Suchmann <pavel.suchmann@gmail.com>
2009-10-11 23:23:26 +0200

    LHSC + wrapping bugfixed

--- geret/lib/crossover_lhs.rb
+++ geret/lib/crossover_lhs.rb
@@ -27,6 +27,11 @@ module Operator
       offspring1 = parent1.clone
       offspring2 = parent2.clone
 
+      # handle possible wrapping of the chromozome (track node info indices bigger than the actual length)
+      offspring1.concat parent1 while offspring1.size < track1.last.to+1
+      offspring2.concat parent2 while offspring2.size < track2.last.to+1
+
+      # organize track nodes by symbols
       hash1 = {}
       track1.each do |node| 
         choices = hash1.fetch( node.symbol, [] )
@@ -40,17 +45,20 @@ module Operator
         hash2[node.symbol] = choices 
       end
 
+      # randomly select the symbol from all _common_ symbols
       symbols = Set.new( hash1.keys ).intersection( hash2.keys ) # extract only symbols present in both tracks
       return offspring1, offspring2 if symbols.empty? # no common symbols -> cloning fallback
       sym = symbols.to_a.sort[ @random.rand( symbols.size ) ] # select a random one
 
+      # randomly select trace nodes by the (compatible) symbol
       choices = hash1[ sym ]
       choice1 = choices[ @random.rand( choices.size ) ]
       choices = hash2[ sym ]
       choice2 = choices[ @random.rand( choices.size ) ]
 
-      part1 = parent1[ choice1.from .. choice1.to ]
-      part2 = parent2[ choice2.from .. choice2.to ]
+      # swap parts
+      part1 = offspring1[ choice1.from .. choice1.to ]
+      part2 = offspring2[ choice2.from .. choice2.to ]
       offspring1[ choice1.from .. choice1.to ] = part2
       offspring2[ choice2.from .. choice2.to ] = part1
       return offspring1, offspring2
