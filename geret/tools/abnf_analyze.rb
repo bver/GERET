384d14da71f7470772448da31a7ce8def6307b27
Pavel Suchmann <pavel.suchmann@gmail.com>
2009-10-13 19:44:55 +0200

    recursivity analysed

--- geret/tools/abnf_analyze.rb
+++ geret/tools/abnf_analyze.rb
@@ -3,18 +3,27 @@
 require 'lib/abnf_file'
 require 'lib/validator'
 
+def format_syms symbols
+  symbols.map{|s| "<#{s}>"}.join(', ')
+end
+
 ###
 
 abort "use:\n #$0 some.abnf\n" unless ARGV.size==1
 
 begin
 
-  grammar = Abnf::File.new ARGV[0]
-  puts "start symbol: <#{grammar.start_symbol}>"
-  undefined = Mapper::Validator.check_undefined grammar
-  puts "undefined symbols: " + undefined.map{|s| "<#{s}>"}.join(', ')
-  unused = Mapper::Validator.check_unused grammar
-  puts "not referenced symbols: " + unused.map{|s| "<#{s}>"}.join(', ')
+  g = Mapper::Validator.analyze_recursivity( Abnf::File.new ARGV[0] )
+  puts "start symbol: <#{g.start_symbol}>"
+  undefined = Mapper::Validator.check_undefined g
+  puts "undefined symbols: " + format_syms( undefined )
+  unused = Mapper::Validator.check_unused g
+  puts "not referenced symbols: " + format_syms( unused )
+
+  puts ":terminating symbols: " + format_syms( g.symbols.find_all {|s| g[s].recursivity == :terminating } )
+  puts ":cyclic symbols: " + format_syms( g.symbols.find_all {|s| g[s].recursivity == :cyclic } )
+  puts ":infinite symbols: " + format_syms( g.symbols.find_all {|s| g[s].recursivity == :infinite } )
+  puts "grammar is #{g[g.start_symbol].recursivity}."
 
 rescue => msg
   abort msg
