e7429b50abf9697e11ddbcada2eff8f8edc745eb
Pavel Suchmann <pavel.suchmann@gmail.com>
2009-12-14 21:07:06 +0100

    cleaning

--- sample/symbolic_regression/pid.rb
+++ sample/symbolic_regression/pid.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/ruby
 
+# this script generates the data:
+# ./pid.rb > data.csv
+
 x = []
 10.times {x << 0.00}
 300.times {x << 2.00}
@@ -9,22 +12,24 @@ x = []
 Kp = 1.0
 Ki = 0.1
 Kd = 4.0
+Dt = 1.0
+Response = 0.1
 
 previous_error = 0.0
 integral = 0.0 
 actual_position = 0.0
-dt = 1.0
-response = 0.1
 
 x.each do |setpoint|
 
+  # PID controller
   error = setpoint - actual_position
-  integral = integral + error*dt
-  derivative = (error - previous_error) / dt
+  integral = integral + error*Dt
+  derivative = (error - previous_error) / Dt
   output = (Kp*error) + (Ki*integral) + (Kd*derivative)
   previous_error = error
 
-  actual_position += output * response
+  # "environment"
+  actual_position += output * Response
 
   puts "#{output},#{setpoint},#{actual_position}"
 end
