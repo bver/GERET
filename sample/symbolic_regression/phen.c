0d0de74cd5aa287bec6cdff8180da631797b280a
Pavel Suchmann <pavel.suchmann@gmail.com>
2009-12-15 00:40:51 +0100

    PID version

--- sample/symbolic_regression/phen.c
+++ sample/symbolic_regression/phen.c
@@ -3,10 +3,13 @@
 
   size_t r;
   double e = 0.0;
+  double y, v1, v2, v3, v4, v5;
+  y = v1 = v2 = v3 = v4 = v5 = 0.0;
+
   for( r=0; r<rows; r++ ) {
     const double *x = &(data[r*cols]);
     
-    double y = PHENOTYPE;
+    PHENOTYPE
     
     if( ! isfinite(y) || y>1e100 || y<-1e100 ) {
       printf( "%lg\n", 1e100 ); //1.79769e+308 );
