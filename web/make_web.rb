356f5dd52cb87114a50c5073a5f748c272be2c84
Pavel Suchmann <pavel.suchmann@gmail.com>
2010-01-09 22:44:41 +0100

    origin

---
+++ web/make_web.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby -w
+
+# gem instal rdoc
+# gem install RedCloth
+# emerge graphviz
+
+require 'redcloth'
+Geret = '../'
+
+system "rm -rf lib/ web/"
+system "mkdir web/"
+
+template = IO.read 'template.html'
+
+index = RedCloth.new( IO.read( Geret + 'README.textile' ) ).to_html
+File.open( 'web/index.html', 'w' ) { |f| f.puts template.gsub(/#CONTENT#/, index) }
+
+system "cp -p -r #{Geret}/lib ."
+system "cd lib/ && rdoc --diagram --inline-source --main geret.rb"
+system "mv lib/doc/ web/"
+system "rm -rf lib/"
+system "cp doc.html web/"
+
+system "cp style.css web/"
+
