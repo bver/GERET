f9d1aa2ea78910b797b8d3edae0959efd7c28b41
Pavel Suchmann <pavel.suchmann@gmail.com>
2010-02-18 23:39:22 +0100

    ant_trail_ag evoling

---
+++ sample/ant_trail_ag/ant_evaluate.c
@@ -0,0 +1,13 @@
+
+PHENOTYPE
+
+int main() {
+
+  while( steps < MaxSteps ) {
+    /* phenotype will be placed here: */
+    step();    
+  }
+
+  printf( "%d\n", consumed_food );
+
+}
---
+++ sample/ant_trail_ag/ant_pipe.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/ruby
+
+AntLib = "#{File.dirname(__FILE__)}/ant.c"
+AntMain = "#{File.dirname(__FILE__)}/ant_evaluate.c"
+
+abort "use:\n #$0 ID\n" unless ARGV.size==1
+main = IO.read AntMain 
+lib = IO.read AntLib
+program = "/tmp/tcc_ant_ag_#{ARGV[0]}.c"
+
+code = ''
+$stdin.each do |line|
+ 
+  if /MARKER/ =~ line
+    source = main.gsub( /PHENOTYPE/, code )
+    File.open( program, "w" ) { |f| f.puts lib+source }
+
+    puts %x"tcc -run #{program}"
+    $stdout.flush
+
+    code = ''
+  else
+    code += line
+  end
+
+end
+
