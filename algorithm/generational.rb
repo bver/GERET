4a8e1c06c70554ed3c09a709e2a7aadc6bb023da
Pavel Suchmann <pavel.suchmann@gmail.com>
2010-03-25 12:20:26 +0100

    phenotypic duplicates elimination for generational alg.

--- algorithm/generational.rb
+++ algorithm/generational.rb
@@ -2,11 +2,13 @@
 require 'algorithm/support/algorithm_base'
 require 'algorithm/support/elitism'
 require 'algorithm/support/breed'
+require 'algorithm/support/phenotypic_truncation'
 
 class Generational < AlgorithmBase
 
   include Elitism
   include Breed 
+  include PhenotypicTruncation
 
   def setup config
     super
@@ -24,7 +26,7 @@ class Generational < AlgorithmBase
     parent_population = @selection.select( @population_size - @elite_size, @population )
     new_population = breed_population( parent_population, @population_size - @elite_size )
     new_population.concat elite( @population )
-
+    new_population = eliminate_duplicates new_population if @duplicate_elimination
     @population = new_population
 
     return @report
