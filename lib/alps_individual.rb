eedb4af5a6a592d29c18e024298344d7c49588be
Pavel Suchmann <pavel.suchmann@gmail.com>
2010-03-27 22:51:08 +0100

    documentation written

--- lib/alps_individual.rb
+++ lib/alps_individual.rb
@@ -1,24 +1,38 @@
 
 module Util
 
+  # This class implements a necessary logic for ALPS strategy. See:
+  # http://www.cs.york.ac.uk/rts/docs/GECCO_2006/docs/p815.pdf
   module AlpsIndividual
 
+    # Set the AGE_GAP parameter of the ALPS. Required for AlpsIndividual.age_limits computation.
+    # This is the multiplier of the age_limits serie.
     def AlpsIndividual.age_gap gap
       @@age_gap = gap
       @@limits = nil
     end
 
+    # Set the aging scheme parameter. Required for AlpsIndividual.age_limits computation.   
+    # Allowed values are: 
+    #   :linear .. 1, 2, 3, 4, 5...
+    #   :fibonacci .. 1, 2, 3, 5, 8, 13, 21...
+    #   :polynomial .. 1, 2, 4, 9, 16, 25, 49...
+    #   :exponential .. 1, 2, 4, 8, 16, 32, 64...
+    #
     def AlpsIndividual.aging_scheme scheme
       @@aging_scheme = scheme
       @@limits = nil     
     end
 
+    # Set the maximum number of layers (the requred length of the AlpsIndividual.age_limits serie).
     def AlpsIndividual.layers layers
       raise "AlpsIndividual: not enough layers, needed at least 2" if layers < 2
       @@layers = layers
       @@limits = nil     
     end
 
+    # Get the age limit for each layer. The result is the AlpsIndividual.aging_scheme serie
+    # of the AlpsIndividual.layers length, multiplied by AlpsIndividual.age_gap.
     def AlpsIndividual.age_limits
       return @@limits unless @@limits.nil?
 
@@ -48,16 +62,26 @@ module Util
       return @@limits = s.map { |i| i * @@age_gap }
     end
 
-
+    # The age of the individual. The newly created one has the age 0.
+    # If the genome of the individual is based of the genome(s) of the parent(s), 
+    # AlpsIndividual#parents method is to be used.
+    #
     def age
       @age = 0 unless defined? @age
       @age
     end
     
+    # Compute the AlpsIndividual#age of the individual from ages of it's parent(s).
+    # If p1 and p2 are AlpsIndividual parents, the age will be:
+    #   my.age = 1 + max( p1.age, p2.age )
+    #
     def parents( p1, p2=nil )
       @age = 1 + ( ( p2.nil? or p1.age > p2.age ) ? p1.age : p2.age )
     end
 
+    # Compute the index of the layer the individual belongs to.  
+    # The result depends on AlpsIndividual.age_limits and AlpsIndividual#age.
+    #
     def layer
       AlpsIndividual.age_limits.each_with_index do |max,i|
         return i if self.age <= max
