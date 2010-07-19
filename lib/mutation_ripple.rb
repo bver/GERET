5d1f278f38adda1b7a6822eaa509a729f0b6d715
Pavel Suchmann <pavel.suchmann@gmail.com>
2010-07-19 17:54:58 +0200

    codon-level ripple strikes back

--- lib/mutation_ripple.rb
+++ lib/mutation_ripple.rb
@@ -3,9 +3,10 @@ require 'lib/codon_mod'
 
 module Operator
 
-# Simple mutation. It assumes the source genotype has the form of the Array of numbers.
+# Simple bit-level mutation. It assumes the source genotype has the form of the Array of codons
+# with interpretation given by CodonMod.
 #
-class MutationRipple
+class MutationBitRipple
 
   # Create the mutation operator with default settings.
   def initialize dummy=nil
@@ -33,6 +34,37 @@ class MutationRipple
     mutant = orig.clone
     where = @random.rand( orig.size )
     mutant[ where ] = @codon.mutate_bit( mutant.at(where) )
+    mutant
+  end
+
+end
+
+
+# Simple codon-level mutation. It assumes the source genotype has the form of the Array of numbers.
+#
+class MutationRipple
+
+  # Create the mutation operator with default settings.
+  def initialize dummy=nil, magnitude=nil
+    @random = Kernel
+    @magnitude = magnitude
+  end
+
+  # The source of randomness, used for calling "random.rand( limit )", defaulting to 'Kernel' class.
+  attr_accessor :random
+
+  # The maximal possible value of the mutaton plus 1. If not specified, the maximal value over the original 
+  # genotype values is used.
+  attr_accessor :magnitude
+
+  # Select the random position within the orig vector and mutate it.
+  # The resultant value (of a mutated codon) is a random number in the range 0..magnitude.
+  # Return the mutated copy of the orig. genotype.
+  def mutation( orig, dummy=nil )
+    mutant = orig.clone
+    max = @magnitude.nil? ? mutant.max+1 : @magnitude
+    where = @random.rand( orig.size )
+    mutant[ where ] = @random.rand( max )
     mutant
   end
 
--- test/tc_mutation_ripple.rb
+++ test/tc_mutation_ripple.rb
@@ -8,8 +8,8 @@ include Operator
 
 class TC_MutationRipple < Test::Unit::TestCase
 
-  def test_basic
-    m = MutationRipple.new
+  def test_bit_basic
+    m = MutationBitRipple.new
     assert_equal( 8, m.codon.bit_size )
     m.random = MockRand.new [{6=>3}, {8=>1}]
     orig = [1, 2, 3, 4, 5, 6]
@@ -18,8 +18,8 @@ class TC_MutationRipple < Test::Unit::TestCase
     assert_equal( [1, 2, 3, 4, 5, 6], orig ) 
   end
 
-  def  test_codon
-    m = MutationRipple.new nil
+  def  test_bit_codon
+    m = MutationBitRipple.new nil
     m.codon = Mapper::CodonMod.new(5)
     assert_equal( 5, m.codon.bit_size )   
 
@@ -28,6 +28,30 @@ class TC_MutationRipple < Test::Unit::TestCase
     mutant = m.mutation orig
     assert_equal( [1, 2, 19, 4, 5, 6], mutant )
     assert_equal( [1, 2, 3, 4, 5, 6], orig ) 
+  end
+
+  def test_basic
+    m = MutationRipple.new
+    m.random = MockRand.new [{6=>3}, {7=>2}]
+    orig = [1, 2, 3, 4, 5, 6]
+    mutant = m.mutation( orig, nil )
+    assert_equal( [1, 2, 3, 2, 5, 6], mutant )
+    assert_equal( [1, 2, 3, 4, 5, 6], orig ) 
+  end
+
+  def  test_magnitude
+    m = MutationRipple.new nil, 40
+    assert_equal( 40, m.magnitude )
+    m.magnitude = 100
+    assert_equal( 100, m.magnitude )   
+
+    m.random = MockRand.new [{6=>3}, {100=>42}]
+    orig = [1, 2, 3, 4, 5, 6]
+    mutant = m.mutation orig
+    assert_equal( [1, 2, 3, 42, 5, 6], mutant )
+    assert_equal( [1, 2, 3, 4, 5, 6], orig ) 
+   
+    assert_equal( nil, MutationRipple.new.magnitude )
   end
 
 end
