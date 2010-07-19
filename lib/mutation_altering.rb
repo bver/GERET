fe094fb155addcc7a261352ec7dace41de48e340
Pavel Suchmann <pavel.suchmann@gmail.com>
2010-07-19 18:29:36 +0200

    codon-level altering strikes back, after some DRYing

--- lib/mutation_altering.rb
+++ lib/mutation_altering.rb
@@ -1,8 +1,34 @@
 
 module Operator
 
-  # Common functionality for MutationNodal and MutationStructural classes. 
+  # Common functionality for MutationBitNodal, MutationBitStructural, MutationNodal and 
+  # MutationStructural classes.  
+  #
+  module MutationAlteringCore
+
+    # See a class description.
+    def mutation( orig, track )
+      mutant = orig.clone
+      filtered_track = track.find_all { |node| @grammar[ node.symbol ].sn_altering == filter }
+      return mutant if filtered_track.empty?
+      where = @random.rand( filtered_track.size )
+      index = filtered_track[where].from 
+      mutant[ index ] = get_codon_value( mutant, index )
+      mutant
+    end
+ 
+  end
+
+  # Common functionality for MutationBitNodal and MutationBitStructural classes. 
+  # 
+  # Select the random (nodal/structural filtered) position within the orig vector and mutate it.
+  # The resultant value (of a mutated codon) is bit-mutated by self.codon.mutate_bit method.
+  # Return the mutated copy of the orig. genotype.
+  # track argument is the hint (symbols atteched to positions) obtained from Mapper::Base#track_support 
+  #
   class MutationBitAltering
+
+    include MutationAlteringCore 
 
     # Create the mutation operator.
     # grammar is the valid grammar for distinguishing :structural and :nodal symbols
@@ -24,25 +50,16 @@ module Operator
       @random = rnd
       @codon.random = rnd
     end
-    
-    # Select the random (nodal/structural filtered) position within the orig vector and mutate it.
-    # The resultant value (of a mutated codon) is bit-mutated by self.codon.mutate_bit method.
-    # Return the mutated copy of the orig. genotype.
-    # track argument is the hint (symbols atteched to positions) obtained from Mapper::Base#track_support 
-    #
-    def mutation( orig, track )
-      mutant = orig.clone
-      filtered_track = track.find_all { |node| @grammar[ node.symbol ].sn_altering == filter }
-      return mutant if filtered_track.empty?
-      where = @random.rand( filtered_track.size )
-      index = filtered_track[where].from 
-      mutant[ index ] = @codon.mutate_bit( mutant.at(index) )
-      mutant
-    end
    
+    protected
+
+    def get_codon_value( mutant, index )  
+      @codon.mutate_bit( mutant.at(index) ) 
+    end
+  
   end
 
-  # Structural mutation (see http://portal.acm.org/citation.cfm?id=1570215).
+  # Structural bit-level mutation (see http://portal.acm.org/citation.cfm?id=1570215).
   # It assumes the source genotype has the form of the Array of numbers.
   # Select codons with Token#sn_altering == :structural for mutation.
   # Structural change means the length of genotype->phenotype transformation process CAN change for mutated genotype.
@@ -54,7 +71,7 @@ module Operator
     end
   end
 
-  # Nodal mutation (see http://portal.acm.org/citation.cfm?id=1570215).
+  # Nodal bit-level mutation (see http://portal.acm.org/citation.cfm?id=1570215).
   # It assumes the source genotype has the form of the Array of numbers.
   # Select codons with Token#sn_altering == :nodal for mutation.
   # Nodal change means the length of genotype->phenotype transformation process CANNOT change for mutated genotype. 
@@ -68,7 +85,14 @@ module Operator
 
 
   # Common functionality for MutationNodal and MutationStructural classes. 
+  # Select the random (nodal/structural filtered) position within the orig vector and mutate it.
+  # The resultant value (of a mutated codon) is a random number in the range 0..magnitude.
+  # Return the mutated copy of the orig. genotype.
+  # track argument is the hint (symbols atteched to positions) obtained from Mapper::Base#track_support 
+  #
   class MutationAltering
+
+    include MutationAlteringCore 
 
     # Create the mutation operator.
     # grammar is the valid grammar for distinguishing :structural and :nodal symbols
@@ -85,27 +109,17 @@ module Operator
     # The maximal possible value of the mutaton plus 1. If not specified, the maximal value over the original 
     # genotype values is used.
     attr_accessor :magnitude
-   
-    
-    # Select the random (nodal/structural filtered) position within the orig vector and mutate it.
-    # The resultant value (of a mutated codon) is a random number in the range 0..magnitude.
-    # Return the mutated copy of the orig. genotype.
-    # track argument is the hint (symbols atteched to positions) obtained from Mapper::Base#track_support 
-    #
-    def mutation( orig, track )
-      mutant = orig.clone
+
+    protected
+
+    def get_codon_value( mutant, index ) 
       max = @magnitude.nil? ? mutant.max+1 : @magnitude
-      filtered_track = track.find_all { |node| @grammar[ node.symbol ].sn_altering == filter }
-      return mutant if filtered_track.empty?
-      where = @random.rand( filtered_track.size )
-      index = filtered_track[where].from 
-      mutant[ index ] = @random.rand( max )
-      mutant
+      @random.rand( max )
     end
-   
+  
   end
 
-  # Structural mutation (see http://portal.acm.org/citation.cfm?id=1570215).
+  # Structural codon-level mutation (see http://portal.acm.org/citation.cfm?id=1570215).
   # It assumes the source genotype has the form of the Array of numbers.
   # Select codons with Token#sn_altering == :structural for mutation.
   # Structural change means the length of genotype->phenotype transformation process CAN change for mutated genotype.
@@ -117,7 +131,7 @@ module Operator
     end
   end
 
-  # Nodal mutation (see http://portal.acm.org/citation.cfm?id=1570215).
+  # Nodal codon-level mutation (see http://portal.acm.org/citation.cfm?id=1570215).
   # It assumes the source genotype has the form of the Array of numbers.
   # Select codons with Token#sn_altering == :nodal for mutation.
   # Nodal change means the length of genotype->phenotype transformation process CANNOT change for mutated genotype. 
