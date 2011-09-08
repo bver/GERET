02260d051cedc3cd44410e38a3c6d1490cf33a62
Pavel Suchmann <pavel@suchmann.cz>
2011-09-08 18:03:38 +0200

    using yardoc instead of rdoc

--- web/make_web.rb
+++ web/make_web.rb
@@ -7,20 +7,14 @@
 require 'redcloth'
 Geret = '../'
 
-system "rm -rf lib/ web/"
+system "rm -rf web/"
 system "mkdir web/"
 
 template = IO.read 'template.html'
-
 index = RedCloth.new( IO.read( Geret + 'README.textile' ) ).to_html
 File.open( 'web/index.html', 'w' ) { |f| f.puts template.gsub(/#CONTENT#/, index) }
 
-system "cp -p -r #{Geret}/lib ."
-system "cd lib/ && rdoc --diagram --inline-source --main geret.rb"
-system "mv lib/doc/ web/"
-system "rm -f web/doc/files/*~.html"
-system "rm -rf lib/"
-system "cp doc.html web/"
+system "yardoc --title=GERET --files='#{Geret}/lib/*.rb' --output-dir=web/ #{Geret}/lib/ #{Geret}/algorithm/ #{Geret}/algorithm/support/"
 
-system "cp style.css web/"
+system "cp style.css doc.html web/"
 
